7da1b7e206d2721a66754cfc7ded03fc03f90b9b
Nikolai Shilenko <nick.shilenko@gmail.com>
2025-10-13 23:20:10 +0300

    feat: Add game state tracking

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
+from .constants import ROOMS
+
+# Состояние игрока и игры
+game_state = {
+    'player_inventory': [],      # Инвентарь игрока
+    'current_room': 'entrance',  # Текущая комната
+    'game_over': False,          # Флаг окончания игры
+    'steps_taken': 0             # Количество шагов
+}
+
 def main():
     print("Первая попытка запустить проект!")
 
